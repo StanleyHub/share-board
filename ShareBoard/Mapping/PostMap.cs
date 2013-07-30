c86f93e1857ce7a8316870a2bf4ec235054a26a5
Xi Chen <xichen@thoughtworks.com>
2013-07-31 00:01:06 +0800

    Add Post model and its mapping to database.

---
+++ ShareBoard/Mapping/PostMap.cs
@@ -0,0 +1,16 @@
+﻿using FluentNHibernate.Mapping;
+using ShareBoard.Models;
+
+namespace ShareBoard.Mapping
+{
+    public class PostMap : ClassMap<Post>
+    {
+        public PostMap()
+        {
+            Id(o => o.Id).GeneratedBy.Assigned();
+            Map(o => o.Title);
+            Map(o => o.Text);
+            Map(o => o.Time);
+        }
+    }
+}
