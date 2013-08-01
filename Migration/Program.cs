81e1d4809530b663470a060b0c5e607a50b84356
Xi Chen <xichen@thoughtworks.com>
2013-08-01 23:52:30 +0800

    Add method to create database session factory.

--- Migration/Program.cs
+++ Migration/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using FluentNHibernate.Cfg;
+﻿using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
 using NHibernate.Cfg;
 using NHibernate.Engine;
---
+++ ShareBoard/DbSessionFactory.cs
@@ -0,0 +1,26 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using ShareBoard.Models;
+
+namespace ShareBoard
+{
+    public class DbSessionFactory
+    {
+        private static ISessionFactory SessionFactory { get; set; }
+
+        public static ISessionFactory GetCurrentFactory()
+        {
+            return SessionFactory ?? (SessionFactory = CreateSessionFactory());
+        }
+
+        private static ISessionFactory CreateSessionFactory()
+        {
+            const string sqlConnectionString = "Data Source=localhost;Initial Catalog=share-board;Integrated Security=True;Enlist=false;";
+            var msSqlConfiguration = MsSqlConfiguration.MsSql2008.ConnectionString(sqlConnectionString).Raw("connection.release_mode", "on_close");
+            return Fluently.Configure().Database(msSqlConfiguration).Mappings(
+                config => config.FluentMappings.AddFromAssembly(typeof(Post).Assembly)).BuildSessionFactory();
+       }
+    }
+}
